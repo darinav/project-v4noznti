ba074775f41b69c38a51e9b17f896ddaf2ddbbad
Daryna Voloshyna <voloshyna.darina@gmail.com>
2025-07-17 16:49:56 +0300

    [wip] guess command logic relative import

--- cli/guess_command/__init__.py
+++ cli/guess_command/__init__.py
@@ -3,8 +3,8 @@
 Пакет guess_command — підказка команд для CLI-помічника.
 """
 
-from cli.guess_command.guess_command import handle_command_with_guess
-from cli.guess_command.possible_commands import CONTACT_COMMANDS, NOTE_COMMANDS
+from .guess_command import handle_command_with_guess
+from .possible_commands import CONTACT_COMMANDS, NOTE_COMMANDS
 
 __all__ = [
     "handle_command_with_guess",
